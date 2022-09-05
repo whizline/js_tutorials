3d009f7bbbdf46d5380ed5a5d193aa0dbb40ef4c
rogers <whizlinerogers@gmail.com>
2022-09-05 19:47:05 +0100

    Intro to js

---
+++ index.js
@@ -0,0 +1,58 @@
+alert ("Welcome")
+
+let a = 12
+let b = 12
+let c = a + b
+
+console.log(c)
+
+//Circumference of a circle 
+//C= 2πr
+const pi =3.142
+const r = 20
+const CircumferenceOfCircle = 2 * pi * r
+console.log(CircumferenceOfCircle)
+
+//Diameter of a Circle
+//D=2*r where r is radius 
+const radius = 10
+const DiameterOfCircle = 2 * radius
+console.log(DiameterOfCircle)
+
+//comparing my answers 
+console.log(CircumferenceOfCircle > DiameterOfCircle)
+console.log(c < DiameterOfCircle)
+
+//Declaring string variables
+const MyFirstName = "Rogers"
+console.log(MyFirstName)
+const MyLastName ="Peter"
+console.log(MyLastName)
+const MyCurrentLocation= "Port Harcourt"
+console.log(MyCurrentLocation)
+const MyLocalGovt = "PHALGA"
+console.log(MyLocalGovt)
+const MyStrongBelief = "Christianity"
+console.log(MyStrongBelief)
+
+//finding the letters at index 5
+console.log(MyFirstName.charAt(5))
+console.log(MyLastName.charAt(5))
+console.log(MyCurrentLocation.charAt(5))
+console.log(MyLocalGovt.charAt(5))
+console.log(MyStrongBelief.charAt(5))
+
+
+//finding the lenght
+console.log(MyFirstName.lenght)
+console.log(MyLastName.lenght)
+console.log(MyCurrentLocation.lenght)
+console.log(MyLocalGovt.lenght)
+console.log(MyStrongBelief.lenght)
+
+//turning all to capital letter 
+console.log(MyFirstName.toUpperCase())
+console.log(MyLastName.toUpperCase())
+console.log(MyCurrentLocation.toUpperCase())
+console.log(MyLocalGovt.toUpperCase())
+console.log(MyStrongBelief.toUpperCase())
